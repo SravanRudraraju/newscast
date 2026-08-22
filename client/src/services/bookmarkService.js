7e7203bfc4f2b8d77e70c5d5911d133c4d119378
SravanRudraraju <sravanrudraraju@gmail.com>
2026-08-22 12:39:52 +0530

    Fix bookmark API endpoint

--- client/src/services/bookmarkService.js
+++ client/src/services/bookmarkService.js
@@ -1,4 +1,4 @@
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api/news`
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/bookmarks`
 
 export const addBookmark = async(bookmark)=>{
     const token = localStorage.getItem("token")
